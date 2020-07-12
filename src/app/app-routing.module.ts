329692cb545a6cdfa2ae371c361f988bf9553eaa
unknown <efredav90@gmail.com>
2020-07-11 22:34:18 -0500

    Examen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TareasComponent } from './componentes/tareas.component';
+import { NotasComponent } from './componentes/notas.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'tareas', component:TareasComponent},
+  {path:'notas', component: NotasComponent},
+  {path:'**',pathMatch:'full', redirectTo:'tareas'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TareasComponent } from './componentes/tareas.component';
+import { NotasComponent } from './componentes/notas.component';
+import { NavbarComponent } from './componentes/navbar.component';
+
+import{ FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TareasComponent,
+    NotasComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
